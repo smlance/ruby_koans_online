bf08c4496b999be2c81b2dce719ad1d95d721501
Pairing Machine <pairing@edgecase.com>
2010-12-23 15:23:17 -0500

    Make it prettier

--- test.rb
+++ test.rb
@@ -3,6 +3,6 @@ require 'sinatra'
 require 'about_arrays'
 
 get '/' do
-  IO.read("about_arrays.rb").gsub(/__/, "<input type='text' />")
+  IO.read("about_arrays.rb").gsub(/__/, "<input type='text' />").gsub(/\n/, "<br />")
 end
 
