79cbcd4b4fe5e0b3ff3c6527e048903f87b7db61
Pairing Machine <pairing@edgecase.com>
2010-12-23 15:15:56 -0500

    Read in file spike

--- test.rb
+++ test.rb
@@ -1,8 +1,14 @@
 require 'rubygems'
 require 'sinatra'
+require 'about_arrays'
 
 get '/' do
-  asdf = 5
-  eval "puts #{params['something']}"
+  file = File.new("about_arrays.rb", "r")
+  counter = 0
+  while (line = file.gets)
+    puts "#{counter}: #{line}"
+    counter = counter + 1
+  end
+  file.close
 end
 
