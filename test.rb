0d4f3e073b0a6b0da226742778701b6fc98dca3e
Pairing Machine <pairing@edgecase.com>
2010-12-23 15:27:34 -0500

    Make code indent

--- test.rb
+++ test.rb
@@ -3,6 +3,6 @@ require 'sinatra'
 require 'about_arrays'
 
 get '/' do
-  IO.read("about_arrays.rb").gsub(/__/, "<input type='text' />").gsub(/\n/, "<br />")
+  IO.read("about_arrays.rb").gsub("\s", "&nbsp;").gsub(/__/, "<input type='text' />").gsub(/\n/, "<br />")
 end
 
