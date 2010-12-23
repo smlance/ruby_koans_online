65f10bdd2e6a4b312ec78f5dbb08b4f87a0e4438
Pairing Machine <pairing@edgecase.com>
2010-12-23 15:43:26 -0500

    splitting headache

--- test.rb
+++ test.rb
@@ -3,6 +3,14 @@ require 'sinatra'
 require 'about_arrays'
 
 get '/' do
-  IO.read("about_arrays.rb").gsub("\s", "&nbsp;").gsub(/__/, "<input type='text' />").gsub(/\n/, "<br />")
+  # IO.read("about_arrays.rb").gsub("\\s", "&nbsp;").gsub(/__/, "<input type='text' />").gsub(/\\n/, "<br />")
+  file = IO.read("about_arrays.rb")
+
+  # index of __def
+  # index of __end + 5
+  start  = file.index('  def')
+  theend = file.index('  end') + 5
+
+  file[start..theend]
 end
 
