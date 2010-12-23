40deeb76cdcb32cae68f583b37627c3a9ce4a58f
Pairing Machine <pairing@edgecase.com>
2010-12-23 15:21:24 -0500

    Replace __ with input tag

--- test.rb
+++ test.rb
@@ -3,12 +3,6 @@ require 'sinatra'
 require 'about_arrays'
 
 get '/' do
-  file = File.new("about_arrays.rb", "r")
-  counter = 0
-  while (line = file.gets)
-    puts "#{counter}: #{line}"
-    counter = counter + 1
-  end
-  file.close
+  IO.read("about_arrays.rb").gsub(/__/, "<input type='text' />")
 end
 
