868d37666dc65e4911216d233286724a610d71b4
Marc Peabody <marc@edgecase.com>
2011-01-11 12:44:13 -0500

    updated to latest koans files (sans edgecase.rb)

--- koans/about_symbols.rb
+++ koans/about_symbols.rb
@@ -11,8 +11,8 @@ class AboutSymbols < EdgeCase::Koan
     symbol2 = :a_symbol
     symbol3 = :something_else
 
-    assert symbol1 == __
-    assert symbol1 != __
+    assert_equal __, symbol1 == symbol2
+    assert_equal __, symbol1 == symbol3
   end
 
   def test_identical_symbols_are_a_single_internal_object
@@ -24,14 +24,14 @@ class AboutSymbols < EdgeCase::Koan
   end
 
   def test_method_names_become_symbols
-    all_symbols = Symbol.all_symbols
-    assert_equal __, all_symbols.include?(:test_method_names_become_symbols)
+    symbols_as_strings = Symbol.all_symbols.map { |x| x.to_s }
+    assert_equal __, symbols_as_strings.include?("test_method_names_become_symbols")
   end
 
   # THINK ABOUT IT:
   #
-  # Why do we capture the list of symbols before we check for the
-  # method name?
+  # Why do we convert the list of symbols to strings and then compare
+  # against the string value rather than against symbols?
 
   in_ruby_version("mri") do
     RubyConstant = "What is the sound of one hand clapping?"
